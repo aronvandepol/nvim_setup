43cda19ecea69dd30906304e07c9f34922b6134a
Aron <vndepol@gmail.com>
2024-03-21 12:53:25 +0100

    updated configs for lsp (added basedpyright) and updated none_ls config to remove depricated lines)

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -38,10 +38,8 @@ return {
 			lspconfig.ruff_lsp.setup({
 				capabilities = capabilities,
 			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
+			lspconfig.basedpyright.setup({
 			})
-
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
